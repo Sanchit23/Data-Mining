f283106f0f423c76422d39c61df65c24f0393579
Sanchit Sindhwani <sanchit2310@gmail.com>
2016-01-28 15:52:28 -0500

    Code for pre processing the files
    Gets the documents  from the .sgm files in the reuters dataset directory
    and extracts information from <topics>, <places>, <title> and <body>
    tags.

--- src/Lab-1/lab1.py
+++ src/Lab-1/lab1.py
@@ -2,16 +2,129 @@ from bs4 import BeautifulSoup
 from os import listdir
 
 dir_path = "/home/0/srini/WWW/674/public/reuters/"
+documents = []
 
+# class for file pre-procesing
 class preProcessor:
-	documents = []
 
+	# method to get documents with raw data from reuters files
 	def getRawDocuments(self):
+		# class variable for keeping track of the number of documents
+		counter = -1
+
+		# iterate over all files in the reuters dataset directory
 		for file in listdir(dir_path):
+			print "\n----------- "+ file +" -----------\n"
+
+			# open file with Beautiful Soup parser
 			xmlFiles = BeautifulSoup(open(dir_path+file),"html.parser")
-			print len(xmlFiles.find_all('topics'))
+			
+			# iterate over each document in file
+			for document in xmlFiles.find_all('reuters'):
+				counter = counter + 1;
+
+				# call methods to extract information from <topics>, <places>, <title> and <body> tags.
+				self.extractTopics(document,counter);
+				self.extractPlaces(document,counter);
+				self.extractTitle(document,counter);
+				self.extractBody(document,counter);
 		return
 
+	# method to extract information from <topics> tag 
+	def extractTopics(self,document,counter):
+		
+		# check if <topic> tag exists
+		if not document.find_all('topics'):
+			documents[counter]["topics"] = ""
+
+		# else, get text from tag(s) 
+		else:
+			
+			for topics in document.find_all('topics'):
+				topic = "";
+				for text in topics.stripped_strings:
+					if topic is "":
+						topic = text
+					else:
+						topic = topic + "," + text;
+
+				#append to the global documents list
+				documents.append({"topics":topic})
+		return
+
+	# method to extract information from <places> tag 
+	def extractPlaces(self,document,counter):
+
+		# check if <topic> tag exists	
+		if not document.find_all('places'):
+			documents[counter]["places"] = ""
+
+		# else, get text from tag(s)
+		else:
+			for places in document.find_all('places'):
+				place = ""
+				if not places.get_text():
+					place = ""
+				else:
+					for text in places.stripped_strings:
+						if place is "":
+							place = text
+						else:
+							place = place + "," + text;
+
+				#append to the global documents list
+				documents[counter]["places"] = place
+		return
+
+	# method to extract information from <title> tag 
+	def extractTitle(self,document,counter):
+
+		# check if <topic> tag exists
+		if not document.find_all('title'):
+			documents[counter]["title"] = ""
+
+		# else, get text from tag(s)
+		else:
+			for titles in document.find_all('title'):
+				title = ""
+				if not titles.get_text():
+					title = ""
+				else:
+					for text in titles.stripped_strings:
+						if title is "":
+							title = text
+						else:
+							title = title + "," + text;
+
+				#append to the global documents list
+				documents[counter]["title"] = title
+		return
+
+	# method to extract information from <body> tag 
+	def extractBody(self,document,counter):
+
+		# check if <body> tag exists	
+		if not document.find_all('body'):
+			documents[counter]["body"] = ""
+		
+		# else, get text from tag(s) 
+		else:
+			for bodies in document.find_all('body'):
+				body = "";
+				if not bodies.get_text():
+					body = ""
+				else:
+					for text in bodies.stripped_strings:
+						if body is "":
+							body = text
+						else:
+							body = body + "," + text;
+
+				#append to the global documents list
+				documents[counter]["body"] = body
+		return
+
+	# method to process raw data
 	def process(self):
 		return
 
