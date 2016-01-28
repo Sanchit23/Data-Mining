9c3c5264bd72cd8b83e1493a7f3aa0cf6ba24482
Sanchit Sindhwani <sanchit2310@gmail.com>
2016-01-28 15:49:25 -0500

    Code for pre processing the files
    Gets the documents  from the .sgm files in the reuters dataset directory
    and extracts information from <topics>, <places>, <title> and <body>
    tags.

--- src/Lab-1/lab1.py
+++ src/Lab-1/lab1.py
@@ -1,1 +1,18 @@
+from bs4 import BeautifulSoup
+from os import listdir
 
+dir_path = "/home/0/srini/WWW/674/public/reuters/"
+
+class preProcessor:
+	documents = []
+
+	def getRawDocuments(self):
+		for file in listdir(dir_path):
+			xmlFiles = BeautifulSoup(open(dir_path+file),"html.parser")
+			print len(xmlFiles.find_all('topics'))
+		return
+
+	def process(self):
+		return
+
+preProcessor().getRawDocuments()
